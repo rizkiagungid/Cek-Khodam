0ec317e7e59df141bc4106440e5aa5b3c0635610
Rizki Agung Sentosa <77620549+rizkiagungid@users.noreply.github.com>
2024-06-20 22:34:14 +0700

    TAMBAH KODAM ANIME LENGKAP

--- script.js
+++ script.js
@@ -30,7 +30,50 @@ document.getElementById('checkButton').addEventListener('click', function() {
         'Kuda Nil Merah', 'Kerbau Emas', 'Jerapah Perak', 'Kumbang Hitam', 'Kupu-kupu Merah', 
         'Kelelawar Merah', 'Singa Laut Hitam', 'Ikan Paus Putih', 'Banteng Hitam', 'Kambing Merah', 
         'Burung Merak Emas', 'Merpati Putih', 'Bebek Merah', 'Burung Hantu Hijau', 'Ayam Jantan', 
-        'Burung Elang Merah', 'Laba-laba Biru','Domba liar','Monyet','Setan Ancol','Saitama','Biju','Seblak', 'Kalajengking Merah', 'Ikan Pari Hitam', 'Burung Hantu Emas','kiyubi','Saint saiya'
+        'Burung Elang Merah', 'Laba-laba Biru','Domba liar','Monyet','Setan Ancol','Saitama','Biju','Seblak', 'Kalajengking Merah', 'Ikan Pari Hitam', 'Burung Hantu Emas','kiyubi','Saint saiya',
+         // Naruto
+    'Naruto Uzumaki', 'Sasuke Uchiha', 'Sakura Haruno', 'Kakashi Hatake', 'Itachi Uchiha', 'Madara Uchiha',
+    'Minato Namikaze', 'Hinata Hyuga', 'Neji Hyuga', 'Gaara', 'Rock Lee', 'Shikamaru Nara',
+    'Ino Yamanaka', 'Choji Akimichi', 'Kiba Inuzuka', 'Shino Aburame', 'Temari', 'Kankuro',
+    // Dragon Ball
+    'Goku', 'Vegeta', 'Gohan', 'Frieza', 'Piccolo', 'Majin Buu', 'Cell', 'Bulma', 'Trunks', 'Krillin',
+    'Yamcha', 'Tien Shinhan', 'Chiaotzu', 'Master Roshi', 'Android 18', 'Android 17', 'Beerus', 'Whis',
+    // One Piece
+    'Luffy', 'Zoro', 'Nami', 'Sanji', 'Usopp', 'Chopper', 'Robin', 'Franky', 'Brook', 'Shanks', 'Ace',
+    'Jimbei', 'Nico Robin', 'Trafalgar Law', 'Donquixote Doflamingo', 'Boa Hancock', 'Buggy', 'Crocodile',
+    // Attack on Titan
+    'Eren Yeager', 'Mikasa Ackerman', 'Armin Arlert', 'Levi Ackerman', 'Erwin Smith', 'Reiner Braun',
+    'Annie Leonhart', 'Hange Zoe', 'Jean Kirstein', 'Connie Springer', 'Sasha Blouse', 'Historia Reiss',
+    // Fairy Tail
+    'Natsu Dragneel', 'Lucy Heartfilia', 'Gray Fullbuster', 'Erza Scarlet', 'Wendy Marvell', 'Happy',
+    'Gajeel Redfox', 'Juvia Lockser', 'Laxus Dreyar', 'Mirajane Strauss', 'Elfman Strauss', 'Lisanna Strauss',
+    // Bleach
+    'Ichigo Kurosaki', 'Rukia Kuchiki', 'Orihime Inoue', 'Uryu Ishida', 'Sado Yasutora', 'Renji Abarai',
+    'Byakuya Kuchiki', 'Toshiro Hitsugaya', 'Kenpachi Zaraki', 'Retsu Unohana', 'Sosuke Aizen', 'Shinji Hirako',
+    // Death Note
+    'Light Yagami', 'L', 'Misa Amane', 'Ryuk', 'Near', 'Mello', 'Soichiro Yagami', 'Touta Matsuda',
+    // Fullmetal Alchemist
+    'Edward Elric', 'Alphonse Elric', 'Roy Mustang', 'Riza Hawkeye', 'Winry Rockbell', 'Maes Hughes',
+    'Alex Louis Armstrong', 'Olivier Mira Armstrong', 'Scar', 'Envy', 'Lust', 'Gluttony', 'Greed',
+    // One Punch Man
+    'Saitama', 'Genos', 'Speed-o\'-Sound Sonic', 'Bang', 'King', 'Tatsumaki', 'Fubuki', 'Mumen Rider',
+    // Hunter x Hunter
+    'Gon Freecss', 'Killua Zoldyck', 'Kurapika', 'Leorio Paradinight', 'Hisoka', 'Chrollo Lucilfer',
+    'Biscuit Krueger', 'Meruem', 'Neferpitou', 'Komugi', 'Isaac Netero', 'Kite', 'Knuckle Bine',
+    // Demon Slayer
+    'Tanjiro Kamado', 'Nezuko Kamado', 'Zenitsu Agatsuma', 'Inosuke Hashibira', 'Kanao Tsuyuri', 'Giyu Tomioka',
+    'Shinobu Kocho', 'Kyojuro Rengoku', 'Tengen Uzui', 'Mitsuri Kanroji', 'Muichiro Tokito', 'Sanemi Shinazugawa',
+    // Black Clover
+    'Asta', 'Yuno', 'Noelle Silva', 'Yami Sukehiro', 'Julius Novachrono', 'Mereoleona Vermillion',
+    'Fuegoleon Vermillion', 'Luck Voltia', 'Magna Swing', 'Vanessa Enoteca', 'Charmy Pappitson', 'Finral Roulacase',
+    // My Hero Academia
+    'Deku', 'Bakugo', 'Todoroki', 'All Might', 'Uraraka', 'Iida', 'Tsuyu Asui', 'Momo Yaoyorozu',
+    'Denki Kaminari', 'Eijiro Kirishima', 'Fumikage Tokoyami', 'Minoru Mineta', 'Katsuki Bakugo', 'Tenya Iida',
+    // Sword Art Online
+    'Kirito', 'Asuna', 'Sinon', 'Leafa', 'Yui', 'Yuuki', 'Eugeo', 'Alice', 'Klein', 'Agil', 'Lisbeth', 'Silica',
+    // Boruto
+    'Boruto Uzumaki', 'Sarada Uchiha', 'Mitsuki', 'Kawaki', 'Shikadai Nara', 'Inojin Yamanaka', 'Chocho Akimichi',
+    'Himawari Uzumaki', 'Konohamaru Sarutobi', 'Moegi Kazamatsuri', 'Udon Ise'
     ];
 
     if (birthDate) {
